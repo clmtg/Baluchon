b4711fcc536ef038def871f84d36e96e08e82aef
Clmtg <garciaclement.c@icloud.com>
2022-04-13 09:43:08 +0200

    Weather city list now sorted alphabetically. Reload default city button added

--- Baluchon/Controller/Weather/WeatherViewController.swift
+++ Baluchon/Controller/Weather/WeatherViewController.swift
@@ -18,6 +18,7 @@ class WeatherViewController: UIViewController {
     //Instance of the Weather data provider model
     private let weather = WeatherService()
     private var cityListCurrentWeather = [weatherStruct]()
+    private var cityListCurrentWeatherSorted = [weatherStruct]()
     private var defaultCityList = [CityStruct(lat: "40.714272", lon: "-74.005966"), //NewYork
                                    CityStruct(lat: "51.966671", lon: "-8.58333"), //Cork
                                    CityStruct(lat: "48.853401", lon: "2.3486"), //Paris
@@ -28,6 +29,13 @@ class WeatherViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet weak var cityListTableView: UITableView!
     @IBOutlet weak var loadingStackView: UIStackView!
+    
+    // MARK: - IBAction
+    @IBAction func tappedReloadButton(_ sender: Any) {
+        cityListCurrentWeather = []
+        cityListTableView.reloadData()
+        collectWeatherDefaultCities()
+    }
     
     // MARK: - Functions
     /// Retreive the weather datas using the model instance.
@@ -59,6 +67,7 @@ class WeatherViewController: UIViewController {
     /// - Parameter weatherData: weather data to display within the TableView
     func addWeatherFor(weatherData: weatherStruct) {
         cityListCurrentWeather.append(weatherData)
+        cityListCurrentWeatherSorted = cityListCurrentWeather.sorted(by: { $0.name < $1.name })
         cityListTableView.reloadData()
     }
 }
@@ -68,7 +77,7 @@ class WeatherViewController: UIViewController {
 extension WeatherViewController: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cityListCurrentWeather.count
+        return cityListCurrentWeatherSorted.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -76,7 +85,7 @@ extension WeatherViewController: UITableViewDataSource{
             return UITableViewCell()
         }
         let index = indexPath.row
-        let affectedCity = cityListCurrentWeather[index]
+        let affectedCity = cityListCurrentWeatherSorted[index]
         cell.configure(for: affectedCity.name, condition: affectedCity.weather[0].description, temp: "\(affectedCity.main["temp"]!)°C (Min: \(affectedCity.main["temp_min"]!)°C Max: \(affectedCity.main["temp_max"]!)°C)")
         return cell
     }
@@ -87,7 +96,7 @@ extension WeatherViewController: UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            cityListCurrentWeather.remove(at: indexPath.row)
+            cityListCurrentWeatherSorted.remove(at: indexPath.row)
             cityListTableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
